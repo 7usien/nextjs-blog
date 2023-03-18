22fced9eafde8b876582eabf78527df35b665edd
7usien <husienadel2022@gmail.com>
2023-03-18 21:10:33 +0300

    update date

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Layout from '../../components/layout';
 import { getPostData } from '../../lib/posts';
 import getAllPostsIds from './../../lib/getAllPostsIds';
-import Date from '../../components/date';
+import Date from '../../components/Date';
 import utilStyles from '../../styles/utils.module.css';
 
 export async function getStaticPaths() {
